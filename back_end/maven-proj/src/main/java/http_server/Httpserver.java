f7b955b763ff7cf0004ebc01c77ae2c831e871c6
Wojciech Lapacz <wojciech.lapacz02@gmail.com>
2022-12-05 17:32:56 +0100

    tried to handle GET request

--- back_end/maven-proj/src/main/java/http_server/Httpserver.java
+++ back_end/maven-proj/src/main/java/http_server/Httpserver.java
@@ -5,7 +5,9 @@ import com.sun.net.httpserver.HttpHandler;
 
 import java.io.IOException;
 import java.io.OutputStream;
+import java.lang.StringBuilder;
 import java.net.InetSocketAddress;
+import org.apache.commons.text.StringEscapeUtils;
 
 
 public class Httpserver {
@@ -18,12 +20,52 @@ public class Httpserver {
 
     static class Handler implements HttpHandler {
         @Override
-        public void handle(HttpExchange t) throws IOException {
-            String response = "Test response";
-            t.sendResponseHeaders(200, response.length());
-            OutputStream os = t.getResponseBody();
-            os.write(response.getBytes());
-            os.close();
+        public void handle(HttpExchange httpExchange) throws IOException {
+            String requestParamValue = null;
+            System.out.println("1");
+            if("GET".equals(httpExchange.getRequestMethod())) {
+                System.out.println("2");
+                requestParamValue = handleGetRequest(httpExchange);
+                System.out.println("3");
+            }
+//            else if("POST".equals(httpExchange)) {
+//                requestParamValue = handlePostRequest(httpExchange);
+//            }
+            handleResponse(httpExchange, requestParamValue);
+        }
+        private String handleGetRequest(HttpExchange httpExchange) {
+            System.out.println(httpExchange.getRequestURI().toString().split("\\?")[1].split("=")[1]);
+            return httpExchange.getRequestURI().toString().split("\\?")[1].split("=")[1];
+        }
+        private void handleResponse(HttpExchange httpExchange, String requestParamValue) throws IOException {
+            OutputStream outputStream = httpExchange.getResponseBody();
+            StringBuilder htmlBuilder = new StringBuilder();
+            htmlBuilder.append("<html>").
+            append("<body>").
+            append("<h1>").
+            append("Hello ")
+                    .append(requestParamValue)
+                    .append("</h1>")
+                    .append("</body>")
+                    .append("</html>");
+            // encode HTML content
+            String htmlResponse = StringEscapeUtils.escapeHtml4(htmlBuilder.toString());
+
+            // this line is a must
+            httpExchange.sendResponseHeaders(200, htmlResponse.length());
+            outputStream.write(htmlResponse.getBytes());
+            outputStream.flush();
+            outputStream.close();
         }
     }
+//    static class Handler implements HttpHandler {
+//        @Override
+//        public void handle(HttpExchange t) throws IOException {
+//            String response = "Test response";
+//            t.sendResponseHeaders(200, response.length());
+//            OutputStream os = t.getResponseBody();
+//            os.write(response.getBytes());
+//            os.close();
+//        }
+//    }
 }
