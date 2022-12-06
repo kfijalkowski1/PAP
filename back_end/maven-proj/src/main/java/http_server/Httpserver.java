780958ed0abc7b280d2f54d5661ae070074e87d4
gambolkf <gambolkf@gmail.com>
2022-12-06 14:01:47 +0100

    small changes

--- back_end/maven-proj/src/main/java/http_server/Httpserver.java
+++ back_end/maven-proj/src/main/java/http_server/Httpserver.java
@@ -1,10 +1,14 @@
 package http_server;
+import com.sun.net.httpserver.Headers;
 import com.sun.net.httpserver.HttpServer;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 
-import java.io.IOException;
-import java.io.OutputStream;
+import java.io.*;
+import java.net.URI;
+import java.util.Scanner; // Import the Scanner class to read text files
+
+
 import java.lang.StringBuilder;
 import java.net.InetSocketAddress;
 import org.apache.commons.text.StringEscapeUtils;
@@ -13,59 +17,125 @@ import org.apache.commons.text.StringEscapeUtils;
 public class Httpserver {
     public static void main(String[] args) throws Exception {
         HttpServer server = HttpServer.create(new InetSocketAddress(8000), 0);
-        server.createContext("/test", new Handler());
+        server.createContext("/", new Handler());
         server.setExecutor(null);
         server.start();
+        // localhost:8000/home//?path="test.html"
     }
 
     static class Handler implements HttpHandler {
-        @Override
-        public void handle(HttpExchange httpExchange) throws IOException {
-            String requestParamValue = null;
-            System.out.println("1");
-            if("GET".equals(httpExchange.getRequestMethod())) {
-                System.out.println("2");
-                requestParamValue = handleGetRequest(httpExchange);
-                System.out.println("3");
-            }
-//            else if("POST".equals(httpExchange)) {
-//                requestParamValue = handlePostRequest(httpExchange);
-//            }
-            handleResponse(httpExchange, requestParamValue);
-        }
-        private String handleGetRequest(HttpExchange httpExchange) {
-            System.out.println(httpExchange.getRequestURI().toString().split("\\?")[1].split("=")[1]);
-            return httpExchange.getRequestURI().toString().split("\\?")[1].split("=")[1];
-        }
-        private void handleResponse(HttpExchange httpExchange, String requestParamValue) throws IOException {
-            OutputStream outputStream = httpExchange.getResponseBody();
-            StringBuilder htmlBuilder = new StringBuilder();
-            htmlBuilder.append("<html>").
-            append("<body>").
-            append("<h1>").
-            append("Hello ")
-                    .append(requestParamValue)
-                    .append("</h1>")
-                    .append("</body>")
-                    .append("</html>");
-            // encode HTML content
-            String htmlResponse = StringEscapeUtils.escapeHtml4(htmlBuilder.toString());
+        public void handle(HttpExchange t) throws IOException {
 
-            // this line is a must
-            httpExchange.sendResponseHeaders(200, htmlResponse.length());
-            outputStream.write(htmlResponse.getBytes());
-            outputStream.flush();
-            outputStream.close();
+            if ("GET".equals(t.getRequestMethod())) {
+                handleGetRequest(t);
+            } else if ("POST".equals(t.getRequestMethod())) {
+                handlePostRequest(t);
+            }
+        }
+
+        public void handlePostRequest(HttpExchange t) {
+            // endpoints
+
+        }
+        public void handleGetRequest(HttpExchange t) throws IOException{
+            String root = "src/main/java/http_server";
+            URI uri = t.getRequestURI();
+            System.out.println("looking for: " + root + uri.getPath());
+            String path = uri.getPath();
+            File file = new File(root + path).getCanonicalFile();
+
+            if (!file.isFile()) {
+                // Object does not exist or is not a file: reject with 404 error.
+                String response = "404 (Not Found)\n";
+                t.sendResponseHeaders(404, response.length());
+                OutputStream os = t.getResponseBody();
+                os.write(response.getBytes());
+                os.close();
+            } else {
+                // Object exists and is a file: accept with response code 200.
+                String mime = "text/html";
+                if (path.substring(path.length() - 3).equals(".js")) mime = "application/javascript";
+                if (path.substring(path.length() - 3).equals("css")) mime = "text/css";
+
+                Headers h = t.getResponseHeaders();
+                h.set("Content-Type", mime);
+                t.sendResponseHeaders(200, 0);
+
+                OutputStream os = t.getResponseBody();
+                FileInputStream fs = new FileInputStream(file);
+                final byte[] buffer = new byte[0x10000];
+                int count = 0;
+                while ((count = fs.read(buffer)) >= 0) {
+                    os.write(buffer, 0, count);
+                }
+                fs.close();
+                os.close();
+            }
         }
     }
+}
+
 //    static class Handler implements HttpHandler {
 //        @Override
-//        public void handle(HttpExchange t) throws IOException {
-//            String response = "Test response";
-//            t.sendResponseHeaders(200, response.length());
-//            OutputStream os = t.getResponseBody();
-//            os.write(response.getBytes());
-//            os.close();
+//        public void handle(HttpExchange httpExchange) throws IOException {
+//            String requestParamValue = null;
+//            if("GET".equals(httpExchange.getRequestMethod())) {
+//                requestParamValue = handleGetRequest(httpExchange);
+//            }
+////            else if("POST".equals(httpExchange)) {
+////                requestParamValue = handlePostRequest(httpExchange);
+////            }
+//            handleResponse(httpExchange, requestParamValue);
+//        }
+//        private String handleGetRequest(HttpExchange httpExchange) {
+//            System.out.println(httpExchange.getRequestURI().toString().split("\\?")[1].split("=")[1]);
+//            return httpExchange.getRequestURI().toString().split("\\?")[1].split("=")[1];
+//        }
+//        private void handleResponse(HttpExchange httpExchange, String requestParamValue) throws IOException {
+//            OutputStream outputStream = httpExchange.getResponseBody();
+//            StringBuilder htmlBuilder = new StringBuilder();
+//            try {
+//                File myObj = new File(requestParamValue);
+//                Scanner myReader = new Scanner(myObj);
+//                while (myReader.hasNextLine()) {
+//                    String data = myReader.nextLine();
+//                    System.out.println(data);
+//                    htmlBuilder.append(data);
+//                }
+//                myReader.close();
+//            } catch (FileNotFoundException e) {
+//                System.out.println("An error occurred.");
+//                e.printStackTrace(); }
+//
+//
+//
+////            htmlBuilder.append("<html>").
+////            append("<body>").
+////            append("<h1>").
+////            append("Hello ")
+////                    .append(requestParamValue)
+////                    .append("</h1>")
+////                    .append("</body>")
+////                    .append("</html>");
+//
+//            // encode HTML content
+//            String htmlResponse = StringEscapeUtils.escapeHtml4(htmlBuilder.toString());
+//
+//            // this line is a must
+//            httpExchange.sendResponseHeaders(200, htmlResponse.length());
+//            outputStream.write(htmlResponse.getBytes());
+//            outputStream.flush();
+//            outputStream.close();
 //        }
 //    }
-}
+////    static class Handler implements HttpHandler {
+////        @Override
+////        public void handle(HttpExchange t) throws IOException {
+////            String response = "Test response";
+////            t.sendResponseHeaders(200, response.length());
+////            OutputStream os = t.getResponseBody();
+////            os.write(response.getBytes());
+////            os.close();
+////        }
+////    }
+//}
