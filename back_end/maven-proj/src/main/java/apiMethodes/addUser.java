70097e3517c4863edd3b5bbe0faa36801e9b64e6
gambolkf <gambolkf@gmail.com>
2022-12-30 17:21:34 +0100

    corrected adding user process

--- back_end/maven-proj/src/main/java/apiMethodes/addUser.java
+++ back_end/maven-proj/src/main/java/apiMethodes/addUser.java
@@ -2,29 +2,32 @@ package apiMethodes;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.json.JSONArray;
 import org.json.JSONObject;
 
 import java.sql.SQLException;
-import java.util.ArrayList;
+import static hashingHandler.PasswordHashing.generateHash;
 
 import static jdbc_handler.jdbc_exp.executeQuery;
 
 public class addUser implements ApiMethodes {
     private static final Logger logger = LogManager.getLogger(Users.class);
     public JSONObject run(JSONObject request) {
-        String value = request.getString("login");
-        logger.info("Adding user: " + value);
-        String query1 = "INSERT INTO users (login, passwd_hash) values ('" + value + "', '2321')";
+        JSONObject result = new JSONObject();
+        String login = request.getString("login");
+        String password = request.getString("password");
+        logger.info("Adding user: " + login);
+        String hash = generateHash(password);
+        String query1 = "INSERT INTO users (login, passwd_hash) values ('" + login + "', '" + hash + "')";
 
         try {
             executeQuery(query1);
             logger.info("User added");
+            result.put("success", true);
         } catch (SQLException e) {
-            //TODO inform frontend about exception
+            result.put("success", false);
             logger.error("Problem with database");
         }
-        return new JSONObject();
+        return result;
     }
 }
 
