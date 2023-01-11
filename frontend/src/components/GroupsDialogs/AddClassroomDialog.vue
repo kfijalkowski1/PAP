7e3df4806f6a733b0278a43f032762cb3f39c43c
Adam Jeliński <adas.jelinski@gmail.com>
2023-01-12 00:11:42 +0100

    Group addition dialogs

---
+++ frontend/src/components/GroupsDialogs/AddClassroomDialog.vue
@@ -0,0 +1,58 @@
+<script setup>
+import { addClassroom, getClassrooms } from '@/api'
+import { errorCatcher } from '@/utils'
+import { defineProps, defineEmits } from 'vue'
+import { $ref } from 'vue/macros'
+
+const props = defineProps(['modelValue', 'facultyId'])
+const emit = defineEmits(['update:modelValue', 'setNewValue', 'setNewOptions'])
+
+let classNr = $ref(null)
+
+const cancel = () => {
+    classNr = null
+
+    emit('update:modelValue', false)
+}
+const submit = errorCatcher(async () => {
+    await addClassroom(classNr, props.facultyId)
+    const newOptions = await getClassrooms(props.facultyId)
+
+    emit('setNewOptions', newOptions)
+    emit('setNewValue', classNr)
+    emit('update:modelValue', false)
+})
+</script>
+
+<template>
+    <v-dialog
+        :model-value="props.modelValue"
+        @update:model-value="emit('update:modelValue', $event)"
+    >
+        <div class="dialogWrapper">
+            <v-card class="dialog">
+                <v-text-field v-model="classNr" label="Number" />
+
+                <div style="float: right">
+                    <v-btn style="margin: 4px" @click="cancel">Cancel</v-btn>
+                    <v-btn style="margin: 4px" color="primary" @click="submit">
+                        Add
+                    </v-btn>
+                </div>
+            </v-card>
+        </div>
+    </v-dialog>
+</template>
+
+<style scoped>
+.dialogWrapper {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+}
+.dialog {
+    flex-grow: 1;
+    max-width: 600px;
+    padding: 32px;
+}
+</style>
---
+++ frontend/src/components/GroupsDialogs/AddGroupNrDialog.vue
@@ -0,0 +1,95 @@
+<script setup>
+import { addGroup, getGroups } from '@/api'
+import { errorCatcher } from '@/utils'
+import { defineProps, defineEmits } from 'vue'
+import { $ref } from 'vue/macros'
+import { dayStrings } from '@/assets'
+
+const props = defineProps([
+    'modelValue',
+    'classroomId',
+    'lecturerId',
+    'courseId',
+])
+const emit = defineEmits(['update:modelValue', 'setNewValue', 'setNewOptions'])
+
+let groupNr = $ref(null)
+let day = $ref(null)
+let timeStart = $ref(null)
+let timeEnd = $ref(null)
+
+const convertTime = (val) => {
+    const [hours, minutes] = val.split(':').map((val) => parseInt(val))
+    return 60 * hours + minutes
+}
+
+const cancel = () => {
+    groupNr = null
+    day = null
+    timeStart = null
+    timeEnd = null
+
+    emit('update:modelValue', false)
+}
+
+const submit = errorCatcher(async () => {
+    await addGroup(
+        groupNr,
+        props.courseId,
+        props.lecturerId,
+        props.classroomId,
+        day,
+        convertTime(timeStart),
+        convertTime(timeEnd)
+    )
+    const newOptions = await getGroups(props.courseId)
+
+    let id = newOptions.find((val) => val.groupNr === groupNr).groupId
+
+    emit('setNewOptions', newOptions)
+    emit('setNewValue', id)
+    emit('update:modelValue', false)
+})
+</script>
+
+<template>
+    <v-dialog
+        :model-value="props.modelValue"
+        @update:model-value="emit('update:modelValue', $event)"
+    >
+        <div class="dialogWrapper">
+            <v-card class="dialog">
+                <v-select
+                    v-model="day"
+                    label="Day"
+                    :items="dayStrings.map((name, value) => ({ name, value }))"
+                    item-title="name"
+                    item-value="value"
+                />
+
+                <v-text-field v-model="startTime" label="Start time" />
+                <v-text-field v-model="endTime" label="End time" />
+
+                <div style="float: right">
+                    <v-btn style="margin: 4px" @click="cancel">Cancel</v-btn>
+                    <v-btn style="margin: 4px" color="primary" @click="submit">
+                        Add
+                    </v-btn>
+                </div>
+            </v-card>
+        </div>
+    </v-dialog>
+</template>
+
+<style scoped>
+.dialogWrapper {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+}
+.dialog {
+    flex-grow: 1;
+    max-width: 600px;
+    padding: 32px;
+}
+</style>
