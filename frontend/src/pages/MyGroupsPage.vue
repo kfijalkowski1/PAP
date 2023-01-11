58b7f32139178c51c52b4bef2c404b81c9e078c3
Adam Jeliński <adas.jelinski@gmail.com>
2023-01-12 00:12:01 +0100

    Polished groups page

--- frontend/src/pages/MyGroupsPage.vue
+++ frontend/src/pages/MyGroupsPage.vue
@@ -1,60 +1,70 @@
 <script setup>
-import { onMounted } from 'vue'
+import { onMounted, watch } from 'vue'
 import { $ref } from 'vue/macros'
-import { getFaculties, getLecturers } from '@/api'
+import {
+    getClassrooms,
+    getCourses,
+    getFaculties,
+    getLecturers,
+    getGroups,
+    getUserGroups,
+    addUserGroup,
+} from '@/api'
 import { errorCatcher } from '@/utils'
 import AddLecturerDialog from '@/components/GroupsDialogs/AddLecturerDialog.vue'
+import AddCourseDialog from '@/components/GroupsDialogs/AddCourseDialog.vue'
+import AddClassroomDialog from '@/components/GroupsDialogs/AddClassroomDialog.vue'
+import AddGroupNrDialog from '@/components/GroupsDialogs/AddGroupNrDialog.vue'
+
+let userGroups = $ref([])
 
 let faculty = $ref(null)
-let facultyOptions = $ref([
-    { name: 'WEITI', id: 1 },
-    { name: 'SIMR', id: 2 },
-    { name: 'MEL', id: 3 },
-])
+let facultyOptions = $ref([])
 
 let lecturer = $ref(null)
-let lecturerOptions = $ref([
-    { name: 'WEITI', id: 1 },
-    { name: 'SIMR', id: 2 },
-    { name: 'MEL', id: 3 },
-])
+let lecturerOptions = $ref([])
 let addLecturerDialog = $ref(false)
 
-/*
+let course = $ref(null)
+let courseOptions = $ref([])
+let addCourseDialog = $ref(false)
 
 let classroom = $ref(null)
-let classroomOptions = $ref([
-    { name: 'WEITI', id: 1 },
-    { name: 'SIMR', id: 2 },
-    { name: 'MEL', id: 3 },
-])
-
-let course = $ref(null)
-let courseOptions = $ref([
-    { name: 'WEITI', id: 1 },
-    { name: 'SIMR', id: 2 },
-    { name: 'MEL', id: 3 },
-])
+let classroomOptions = $ref([])
+let addClassroomDialog = $ref(false)
 
 let groupNr = $ref(null)
-let groupNrOptions = $ref([
-    { name: 'WEITI', id: 1 },
-    { name: 'SIMR', id: 2 },
-    { name: 'MEL', id: 3 },
-])
-*/
+let groupNrOptions = $ref([])
+let addGroupNrDialog = $ref(false)
 
 onMounted(
     errorCatcher(async () => {
+        userGroups = await getUserGroups()
+
         facultyOptions = await getFaculties()
         lecturerOptions = await getLecturers()
-        /*
-        classroomOptions = await getClassrooms()
-        courseOptions = await getCourses()
-        groupNrOptions = await getGroups
-        */
     })
 )
+
+watch(
+    faculty,
+    errorCatcher(async () => {
+        classroomOptions = await getClassrooms()
+        courseOptions = await getCourses()
+    })
+)
+
+watch(
+    course,
+    errorCatcher(async () => {
+        groupNrOptions = await getGroups()
+    })
+)
+
+const joinGroup = errorCatcher(async () => {
+    await addUserGroup(groupNr.groupId)
+    userGroups = await getUserGroups()
+})
 </script>
 
 <template>
@@ -70,10 +80,15 @@ onMounted(
                     </tr>
                 </thead>
                 <tbody>
-                    <tr>
-                        <td>WEITI</td>
-                        <td>AISDI</td>
-                        <td>104</td>
+                    <tr v-if="userGroups.length === 0">
+                        <td></td>
+                        <td>No groups :(</td>
+                        <td></td>
+                    </tr>
+                    <tr v-for="group in userGroups" :key="group.groupId">
+                        <td>{{ group.facultyShortname }}</td>
+                        <td>{{ group.courseCode }}</td>
+                        <td>{{ group.groupNr }}</td>
                     </tr>
                 </tbody>
             </v-table>
@@ -108,6 +123,72 @@ onMounted(
                     @setNewOptions="lecturerOptions = $event"
                 />
             </div>
+            <div class="line">
+                <v-autocomplete
+                    label="Classroom"
+                    v-model="classroom"
+                    :items="classroomOptions"
+                    itemTitle="classNr"
+                    itemValue="classNr"
+                />
+                <v-btn
+                    icon="mdi-plus"
+                    size="small"
+                    class="addButton"
+                    @click="addClassroomDialog = true"
+                />
+                <AddClassroomDialog
+                    v-model="addClassroomDialog"
+                    @setNewValue="classroom = $event"
+                    @setNewOptions="classroomOptions = $event"
+                    :facultyId="faculty ? faculty.id : null"
+                />
+            </div>
+            <div class="line">
+                <v-autocomplete
+                    label="Course"
+                    v-model="course"
+                    :items="courseOptions"
+                    itemTitle="name"
+                    itemValue="courseId"
+                />
+                <v-btn
+                    icon="mdi-plus"
+                    size="small"
+                    class="addButton"
+                    @click="addCourseDialog = true"
+                />
+                <AddCourseDialog
+                    v-model="addCourseDialog"
+                    @setNewValue="course = $event"
+                    @setNewOptions="courseOptions = $event"
+                    :facultyId="faculty ? faculty.id : null"
+                />
+            </div>
+            <div class="line">
+                <v-autocomplete
+                    label="Group number"
+                    v-model="groupNr"
+                    :items="groupNrOptions"
+                    itemTitle="groupNr"
+                    itemValue="groupId"
+                />
+                <v-btn
+                    icon="mdi-plus"
+                    size="small"
+                    class="addButton"
+                    @click="addGroupNrDialog = true"
+                />
+                <AddGroupNrDialog
+                    v-model="addGroupNrDialog"
+                    @setNewValue="groupNr = $event"
+                    @setNewOptions="groupNrOptions = $event"
+                    :courseId="course ? course.courseId : null"
+                    :classroomId="classroom ? classroom.id : null"
+                    :lecturerId="lecturer ? lecturer.lecturerId : null"
+                />
+            </div>
+            <v-btn color="primary" @click="joinGroup"> Join group </v-btn>
         </v-sheet>
     </div>
 </template>
